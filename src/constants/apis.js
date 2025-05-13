bbddfcde8f1b9aead6a208c198b62145a5e6e867
ejaz <ejaz.ahmed@codenicely.in>
2025-05-13 19:08:52 +0530

    create: api urls

---
+++ src/constants/apis.js
@@ -0,0 +1,7 @@
+//login
+export const LOGIN = '/user/user_register/'
+export const GET_MEMBERS_LIST = '/project/members/list/'
+export const CREATE_PROJECT = '/project/create_project/'
+export const GET_PROJECTS_LIST = '/project/get_project/list/'
+export const DELETE_PROJECT = '/project/delete_project/:project_id'
+
