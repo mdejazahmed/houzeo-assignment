9c659244220ba56803cd572f68c5b47aa4a29185
ejaz <ejaz.ahmed@codenicely.in>
2025-05-13 19:16:04 +0530

    create: project section

---
+++ src/pages/app/project.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <div class="d-flex justify-space-between">
+      <div>
+        <h5 class="text-h5">Project Name 🚀 👋</h5>
+        <p class="text-subtitle-2 text-medium-emphasis">
+            Keep it up and complete today’s tasks to achieve the highest streak.
+        </p>
+      </div>
+      <div class="d-flex align-center gap-2">
+        <v-btn
+          icon="mdi-magnify"
+          variant="outlined"
+          color="primary"
+          size="small"
+        ></v-btn>
+        <v-btn variant="outlined" color="primary" rounded>Add Team Members</v-btn>
+        <v-btn
+          variant="flat"
+          color="primary"
+          rounded
+          >Add Group</v-btn
+        >
+      </div>
+    </div>
+  </v-container>
+</template>
