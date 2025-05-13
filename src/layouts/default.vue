17395b0162245873d8535218cd9d494607175c54
ejaz <ejaz.ahmed@codenicely.in>
2025-05-13 19:16:44 +0530

    create: default layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,34 +1,73 @@
 <template>
   <v-layout>
-      <v-navigation-drawer
-        expand-on-hover
-        rail
-      >
+    <v-navigation-drawer expand-on-hover>
+      <v-list>
+        <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+          subtitle="sandra_a88@gmailcom"
+          title="Sandra Adams"
+        ></v-list-item>
+      </v-list>
+
+      <v-divider></v-divider>
+
+      <v-list density="compact" nav height="100%" color="primary">
+        <v-list-item
+          v-for="item in drawerItems"
+          :key="item.value"
+          :value="item.value"
+          :prepend-icon="item.icon"
+          :title="item.title"
+        ></v-list-item>
+      </v-list>
+      <template v-slot:append>
         <v-list>
           <v-list-item
-            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
-            subtitle="sandra_a88@gmailcom"
-            title="Sandra Adams"
+          prepend-icon="mdi-logout"
+            title="Logout"
+            value="logout"
+            @click="logout"
           ></v-list-item>
+          <v-list-item prepend-icon="mdi-theme-light-dark" title="Theme">
+            <template v-slot:append>
+              <v-list-item-action>
+               <ToggleTheme />
+              </v-list-item-action>
+            </template>
+          </v-list-item>
         </v-list>
+      </template>
+    </v-navigation-drawer>
 
-        <v-divider></v-divider>
-
-        <v-list density="compact" nav>
-          <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
-          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
-          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-
-    <v-main class="d-flex align-center justify-center" height="300">
+    <v-main>
       <v-container>
-       <router-view />
+        <router-view />
       </v-container>
     </v-main>
   </v-layout>
 </template>
 
 <script setup>
-  //
+import ToggleTheme from '@/components/themeToggle/ToggleTheme.vue'
+import { useAuthStore } from '@/stores/auth'
+const {logout} = useAuthStore()
+const drawerItems = [
+  {
+    title: 'Projects',
+    icon: 'mdi-home-outline',
+    value: 'projects',
+  },
+  {
+    title: 'Team',
+    icon: 'mdi-account-group-outline',
+    value: 'team',
+  },
+  {
+    title: 'Weekly Plan',
+    icon: 'mdi-calendar-week-outline',
+    value: 'weekly_plan',
+  },
+]
+
+
 </script>
