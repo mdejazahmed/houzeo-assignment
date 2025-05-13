35971825007347e637f8308ad269a256ecf971dd
ejaz <ejaz.ahmed@codenicely.in>
2025-05-13 19:17:01 +0530

    create: auth layout

--- src/layouts/auth.vue
+++ src/layouts/auth.vue
@@ -2,21 +2,29 @@
   <v-container
     class="fill-height"
     fluid
+  
   >
     <v-row
-      align="center"
-      justify="center"
+  
+   
     >
       <v-col
-        cols="12"
-        sm="8"
-        md="4"
+        cols="8"
       >
-        <v-card class="elevation-12">
-          <v-card-text class="pa-8">
+       <div class="d-flex flex-column align-center">
+      <v-img
+        src="@/assets/login.svg"
+        max-width="400"
+        class="mx-auto"
+      />
+      <h2>Lorem Ipsum Dolor Amet, Ipsum dolor lorem amet.</h2>
+      <p>Lorem Ipsum Dolor Amet, Ipsum dolor lorem amet, Lorem Ipsum Dolor Amet, Ipsum dolor lorem amet, Lorem Ipsum Dolor Amet, Ipsum dolor lorem amet.</p>
+      </div>
+          </v-col>
+      <v-col
+        cols="4"
+      >
             <router-view />
-          </v-card-text>
-        </v-card>
       </v-col>
     </v-row>
   </v-container>
