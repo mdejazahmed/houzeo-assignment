0362bbdef2af766e14f509cf0d208867ac7024fe
ejaz <ejaz.ahmed@codenicely.in>
2025-05-14 19:08:08 +0530

    modify: api file

--- src/constants/apis.js
+++ src/constants/apis.js
@@ -1,7 +1,19 @@
 //login
 export const LOGIN = '/user/user_register/'
+//home
 export const GET_MEMBERS_LIST = '/project/members/list/'
 export const CREATE_PROJECT = '/project/create_project/'
 export const GET_PROJECTS_LIST = '/project/get_project/list/'
 export const DELETE_PROJECT = '/project/delete_project/:project_id'
+//project
+export const GET_PROJECT = '/project/get_project/details/:project_id'
+//group
+export const CREATE_GROUP = '/project/create_projects_group/'
+export const GET_GROUPS = '/project/get_projects_group/list/:project_id'
+export const GET_PROJECT_TEAMS = '/project/get_project_teams/list/:project_id/'
+//task
+export const CREATE_TASK = '/project/task/create_task/'
+
+
+
 
