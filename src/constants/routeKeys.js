af95ebb3d6528cfa3d0fb4c07fe07e249854c65c
ejaz <ejaz.ahmed@codenicely.in>
2025-05-16 19:13:28 +0530

    create: routes for create weekly plan and show all weekly plan

--- src/constants/routeKeys.js
+++ src/constants/routeKeys.js
@@ -12,5 +12,12 @@ export const ROUTES = {
       name: "project",
       path: "/project/:project_id",
     },  
-    
+    WEEKLY_PLAN: {
+      name: "weekly_plan",
+      path: "/weekly_plan",
+    },
+    CREATE_WEEKLY_PLAN: {
+      name: "create_weekly_plan",
+      path: "/create_weekly_plan/:weekly_plan_id",
+    },
 }   
