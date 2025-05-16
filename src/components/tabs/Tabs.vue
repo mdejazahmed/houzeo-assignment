82d800d9c9d8d978dfee7227dd2f6b131572c912
ejaz <ejaz.ahmed@codenicely.in>
2025-05-16 19:11:06 +0530

    create: tabs component

---
+++ src/components/tabs/Tabs.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { useRoute, useRouter } from "vue-router";
+const route = useRoute();
+const router = useRouter();
+
+const { tabs } = defineProps({
+    tabs: {
+        type: Array,
+        required: true,
+    },
+});
+
+
+
+const emit = defineEmits(['update:modelValue']);
+</script>
+<template>
+    <v-tabs fixed-tabs >
+              <v-tab
+                v-for="tab in tabs"
+                :key="tab.value"
+                :value="tab.value"
+                :ripple="false"
+                variant="text"
+                color="blue"
+                base-color="label"
+              >
+                {{ tab.title }}
+                <span v-if="tab.count"> ({{ tab.count }}) </span>
+              </v-tab>
+            </v-tabs>
+</template>
