2d6d09db0d0bac9df18f3e6b4b72d1df938ba129
ejaz <ejaz.ahmed@codenicely.in>
2025-05-16 19:13:08 +0530

    api integration: get weekly plan tabs and data

--- src/constants/apis.js
+++ src/constants/apis.js
@@ -13,6 +13,10 @@ export const GET_GROUPS = '/project/get_projects_group/list/:project_id'
 export const GET_PROJECT_TEAMS = '/project/get_project_teams/list/:project_id/'
 //task
 export const CREATE_TASK = '/project/task/create_task/'
+//weekly plan
+export const GET_WEEKLY_PLAN_TABS = '/project/weekly_plan/get_project_weekly_plan/tab_list/'
+export const GET_WEEKLY_PLAN_DATA = '/project/weekly_plan/get_weekly_plan/list/'
+
 
 
 
