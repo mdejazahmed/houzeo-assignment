76ae87de41eecb054ba08260c76196f76e8c0c50
ejaz <ejaz.ahmed@codenicely.in>
2025-05-19 17:57:22 +0530

    modify: task card component

--- src/components/cards/TaskCard.vue
+++ src/components/cards/TaskCard.vue
@@ -1,22 +1,74 @@
+<script setup>
+import { useDate } from "vuetify";
+const { task, editable, group_id, project_id } = defineProps({
+  task: {
+    type: Object,
+    required: true,
+  },
+  editable: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+  group_id: {
+    type: String,
+    required: true,
+  },
+  project_id: {
+    type: String,
+    required: true,
+  },
+  movable: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+});
+const date = useDate(task.due_date);
+
+const handleCreateTask = (createdTask = {}) => {
+  emit("success", createdTask);
+};
+
+const emit = defineEmits(["success"]);
+
+const addEditTaskDialog = reactive({
+  id: null,
+});
+const openAddEditTaskDialog = () => (addEditTaskDialog.id = task.id);
+const closeAddEditTaskDialog = () => (addEditTaskDialog.id = null);
+</script>
+
 <template>
+  <AddEditTask
+    v-if="addEditTaskDialog.id"
+    :task="task"
+    @close="closeAddEditTaskDialog"
+    :group_id="group_id"
+    :project_id="project_id"
+    @success="handleCreateTask"
+  />
   <v-card
-    border="dashed"
+    v-else
+    border="thin dashed"
     variant="outlined"
     class="rounded-lg d-flex flex-column gap-2"
+    :hover="editable"
   >
-    <v-card-text>
+    <v-card-text @click="editable ? openAddEditTaskDialog() : null">
       <div class="d-flex flex-column gap-4">
         <div class="d-flex align-center justify-space-between gap-2">
-        <p class="text-h6">
-          {{ task.task }}
-        </p>
-          <v-avatar color="blue" size="x-small" >
+          <p class="text-h6">
+            {{ task.task }}
+          </p>
+          <v-avatar color="blue" size="x-small">
             {{ task.assignees?.email[0].toUpperCase() }}
           </v-avatar>
         </div>
       </div>
       <div class="d-flex align-center gap-2 mt-2">
         <v-chip
+          border="thin dashed"
           :text="date.format(task.due_date, 'fullDateWithWeekday')"
           prepend-icon="mdi-calendar-blank-outline"
           label
@@ -27,6 +79,7 @@
         >
         </v-chip>
         <v-chip
+          border="thin dashed"
           :text="task.duration"
           prepend-icon="mdi-clock-outline"
           label
@@ -45,22 +98,14 @@
             density="compact"
             :color="task.task_priority.color"
             variant="flat"
-            class="border-dashed"
+            class="border-dashed text-white"
           >
           </v-chip>
         </div>
       </div>
     </v-card-text>
+    <v-card-actions v-if="movable">
+      <slot name="actions" :task="task"></slot>
+    </v-card-actions>
   </v-card>
 </template>
-
-<script setup>
-import { useDate } from 'vuetify'
-const {task} = defineProps({
-  task: {
-    type: Object,
-    required: true,
-  },
-});
-const date = useDate(task.due_date);
-</script>
