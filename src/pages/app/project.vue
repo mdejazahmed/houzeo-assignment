2b096f7e599a5eae39b80c88b158c51aa3ffc65d
ejaz <ejaz.ahmed@codenicely.in>
2025-05-19 18:00:01 +0530

    modify: project page UI

--- src/pages/app/project.vue
+++ src/pages/app/project.vue
@@ -1,3 +1,44 @@
+
+<script setup>
+import { reactive } from "vue";
+import { ref } from "vue";
+import request from "@/plugins/axios";
+import { GET_PROJECT } from "@/constants/apis";
+import { useRoute } from "vue-router";
+import AddEditGroupModal from "@/components/dialogs/AddEditGroupModal.vue";
+import GroupCard from "@/components/cards/GroupCard.vue";
+const route = useRoute();
+const addGroupDialog = reactive({
+  show: false,
+  title: "Add Group",
+  data: null,
+});
+const project = ref({});
+const openTaskDialog = reactive({
+  show: false,
+  id: null,
+});
+const closeTaskDialog = () => {
+  openTaskDialog.show = false;
+  openTaskDialog.id = null;
+};
+const handleCreateTask = () => {
+  closeTaskDialog();
+  getProjectDetail();
+};
+const getProjectDetail = async () => {
+  try {
+    const res = await request.get(
+      GET_PROJECT.replace(":project_id", route.params.project_id)
+    );
+    project.value = res.data?.detail;
+  } catch (error) {
+    console.log(error);
+  }
+};
+getProjectDetail();
+</script>
+
 <template>
   <v-container>
     <v-row class="d-flex justify-space-between">
@@ -7,7 +48,13 @@
           {{ project.description }}
         </p>
       </v-col>
-      <v-col cols="12" sm="6" md="4" lg="6" class="d-flex align-center justify-end gap-2">
+      <v-col
+        cols="12"
+        sm="6"
+        md="4"
+        lg="6"
+        class="d-flex align-center justify-end gap-2"
+      >
         <v-btn
           icon="mdi-magnify"
           variant="outlined"
@@ -28,13 +75,71 @@
     </v-row>
 
     <v-row>
-      <v-col
-        v-for="group in project.project_groups"
-        :key="group.id"
-        cols="12"
-        
-      >
-        <GroupCard :group="group" @success="getProjectDetail"/>
+      <v-col v-for="group in project.project_groups" :key="group.id" cols="12">
+        <GroupCard :group="group">
+          <section>
+            <p class="text-h6" v-if="group?.pending_tasks?.length">
+              Pending Tasks
+            </p>
+            <v-list v-if="group?.pending_tasks?.length">
+              <v-list-item v-for="task in group?.pending_tasks" :key="task.id">
+                <TaskCard
+                  :editable="true"
+                  :task="task"
+                  :group_id="group.id"
+                  :project_id="project.id"
+                  @success="handleCreateTask"
+                />
+              </v-list-item>
+              <v-list-item>
+                <AddEditTask
+              v-if="openTaskDialog.show"
+              @close="closeTaskDialog"
+              :project_id="project.id"
+              :group_id="group.id"
+              @success="handleCreateTask"
+            />
+            <v-btn
+              v-if="!openTaskDialog.show"
+              variant="outlined"
+              size="small"
+              rounded
+              @click="openTaskDialog.show = true"
+              >Add a task</v-btn
+            >
+              </v-list-item>
+            </v-list>
+            
+           
+          </section>
+          <section>
+            <p class="text-h6" v-if="group?.wip_tasks?.length">WIP Tasks</p>
+            <v-list v-if="group?.wip_tasks?.length">
+              <v-list-item v-for="task in group?.wip_tasks" :key="task.id">
+                <TaskCard :task="task" 
+                :group_id="group.id"
+                  :project_id="project.id"
+                />
+              </v-list-item>
+            </v-list>
+          </section>
+          <section>
+            <p class="text-h6" v-if="group?.completed_tasks?.length">
+              Completed Tasks
+            </p>
+            <v-list v-if="group?.completed_tasks?.length">
+              <v-list-item
+                v-for="task in group?.completed_tasks"
+                :key="task.id"
+              >
+                <TaskCard :task="task"
+                :group_id="group.id"
+                  :project_id="project.id"
+                />
+              </v-list-item>
+            </v-list>
+          </section>
+        </GroupCard>
       </v-col>
     </v-row>
     <AddEditGroupModal
@@ -47,30 +152,3 @@
   </v-container>
 </template>
 
-<script setup>
-import { reactive } from "vue";
-import { ref } from "vue";
-import request from "@/plugins/axios";
-import { GET_PROJECT } from "@/constants/apis";
-import { useRoute } from "vue-router";
-import AddEditGroupModal from "@/components/dialogs/AddEditGroupModal.vue";
-import GroupCard from "@/components/cards/GroupCard.vue";
-const route = useRoute();
-const addGroupDialog = reactive({
-  show: false,
-  title: "Add Group",
-  data: null,
-});
-const project = ref({});
-const getProjectDetail = async () => {
-  try {
-    const res = await request.get(
-      GET_PROJECT.replace(":project_id", route.params.project_id)
-    );
-    project.value = res.data?.detail;
-  } catch (error) {
-    console.log(error);
-  }
-};
-getProjectDetail();
-</script>
