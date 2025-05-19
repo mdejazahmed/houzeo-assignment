63d92a25b07df1b3f6c639716be1b7372217d9ba
ejaz <ejaz.ahmed@codenicely.in>
2025-05-19 17:57:00 +0530

    refactor: group card component

--- src/components/cards/GroupCard.vue
+++ src/components/cards/GroupCard.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, reactive } from "vue";
+import { ref, reactive, computed } from "vue";
 import { GET_PROJECT_TEAMS, CREATE_TASK } from "@/constants/apis";
 import request from "@/plugins/axios";
 import UserChip from "@/components/chips/UserChip.vue";
@@ -10,6 +10,10 @@ import AddEditTask from "@/components/dialogs/AddEditTask.vue";
 const route = useRoute();
 const props = defineProps({
   group: Object,
+  showProgress: {
+    type: Boolean,
+    default: false,
+  },
 });
 const emit = defineEmits(["success"]);
 
@@ -26,6 +30,13 @@ const handleCreateTask = () => {
   closeTaskDialog();
   emit("success");
 };
+const progress = computed(() => {
+  return (
+    (props.group?.task_counts?.completed_tasks /
+      props.group?.task_counts?.total_tasks) *
+    100
+  );
+});
 </script>
 
 <template>
@@ -38,86 +49,21 @@ const handleCreateTask = () => {
 
     <v-card class="rounded-lg mt-2">
       <v-card-title :title="props.group?.name">
-        <div class="d-flex align-center gap-2 justify-space-between">
+        <div class="d-flex align-center gap-2 justify-space-between" v-if="props.showProgress">
           <v-progress-linear
             color="green"
             height="10"
-            :model-value="
-              (props.group?.task_counts.completed_tasks /
-                props.group?.task_counts.total_tasks) *
-              100
-            "
+            :model-value="progress"
             rounded
           ></v-progress-linear>
           <span class="text-subtitle-2"
-            >{{ props.group?.task_counts.completed_tasks }} /
-            {{ props.group?.task_counts.total_tasks }}</span
+            >{{ props.group?.task_counts?.completed_tasks }} /
+            {{ props.group?.task_counts?.total_tasks }}</span
           >
         </div>
       </v-card-title>
       <v-card-text>
-        <section>
-          <!-- Pending Tasks -->
-          <p class="text-h6" v-if="props.group?.pending_tasks?.length">Pending Tasks</p>
-          <v-list v-if="props.group?.pending_tasks?.length">
-            <v-list-item
-              v-for="task in props.group?.pending_tasks"
-              :key="task.id"
-              :title="task.name"
-            >
-              <AddEditTask
-                v-if="task.id == openTaskDialog.id"
-                :task="task"
-                @close="closeTaskDialog"
-                :group_id="props.group.id"
-                @success="handleCreateTask"
-              />
-              <TaskCard
-                v-else
-                :task="task"
-                @click="openTaskDialog.id = task.id"
-              />
-            </v-list-item>
-          </v-list>
-          <AddEditTask
-                v-if="openTaskDialog.show"
-                @close="closeTaskDialog"
-                :project_id="props.group.project_id"
-                :group_id="props.group.id"
-                @success="handleCreateTask"
-              />
-              <v-btn
-                v-if="!openTaskDialog.show"
-                variant="outlined"
-                size="small"
-                rounded
-                @click="openTaskDialog.show = true"
-                >Add a task</v-btn
-              >
-
-          <!-- WIP Tasks -->
-          <p class="text-h6" v-if="props.group?.wip_tasks?.length">WIP Tasks</p>
-          <v-list v-if="props.group?.wip_tasks?.length">
-            <v-list-item
-              v-for="task in props.group?.wip_tasks"
-              :key="task.id"
-              :title="task.name"
-            >
-              <TaskCard :task="task" />
-            </v-list-item>
-          </v-list>
-          <!-- Completed Tasks -->
-          <p class="text-h6" v-if="props.group?.completed_tasks?.length">Completed Tasks</p>
-          <v-list v-if="props.group?.completed_tasks?.length">
-            <v-list-item
-              v-for="task in props.group?.completed_tasks"
-              :key="task.id"
-              :title="task.name"
-            >
-              <TaskCard :task="task" />
-            </v-list-item>
-          </v-list>
-        </section>
+        <slot></slot>
       </v-card-text>
     </v-card>
   </div>
