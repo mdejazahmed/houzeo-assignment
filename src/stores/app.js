8f4e8b0f64debb818e220ac84d5e31f93e176fa6
Ejaz Ahmed <ejaz.ahmed@codenicely.in>
2025-05-21 19:08:24 +0530

    feature( app store): create snackbar store

--- src/stores/app.js
+++ src/stores/app.js
@@ -6,3 +6,17 @@ export const useAppStore = defineStore('app', {
     //
   }),
 })
+export const useSnackbarStore = defineStore("snackbarStore", {
+  state: () => ({
+    show: false,
+    color:"success",
+    msg:"Operation Successful"
+  }),
+  actions:{
+    showSnackbar({color="success",msg="Operation Successful"}){
+     this.show=true
+     this.color=color;
+     this.msg=msg;
+    },
+  }
+});
