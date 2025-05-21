2b45a5d48fb76fc9fe62d6e6025055a45914d11a
Ejaz Ahmed <ejaz.ahmed@codenicely.in>
2025-05-21 19:10:17 +0530

    feat(toggletheme):- show snackbar when theme toggle

--- src/components/themeToggle/ToggleTheme.vue
+++ src/components/themeToggle/ToggleTheme.vue
@@ -11,15 +11,18 @@
 import { computed } from 'vue'
 import { useThemeStore } from '@/stores/theme'
 import { useTheme } from 'vuetify'
+import { useSnackbarStore } from '@/stores/app'
 
 const themeStore = useThemeStore()
 const theme = useTheme()
+const snackbarStore = useSnackbarStore()
 
 // Use computed to properly track changes
 const isDark = computed(() => themeStore.isDark)
 
 const toggleTheme = () => {
   themeStore.toggleTheme()
+  snackbarStore.showSnackbar({msg:"Theme Changed"})
 }
 
 // Watch for changes and update Vuetify theme
