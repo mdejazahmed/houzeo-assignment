bd01908985bcdc42e00c8023a67f84321bd0dcb2
Ejaz Ahmed <ejaz.ahmed@codenicely.in>
2025-05-21 19:12:38 +0530

    add snackbar componnet globally

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <v-app>
     <component :is="layout" theme="dark">
-    <router-view />
+      <router-view />
     </component>
+    <Snackbar/>
   </v-app>
+
 </template>
 
 <script setup>
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
+import Snackbar from '@/components/snackbar/Snackbar.vue'
 
 const route = useRoute()
 const layout = computed(() => {
