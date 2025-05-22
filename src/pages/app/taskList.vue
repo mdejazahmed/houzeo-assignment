f1a9d8cb362c0e68714c4bdc279e092f61b90746
Ejaz Ahmed <ejaz.ahmed@codenicely.in>
2025-05-22 14:24:20 +0530

    create page for listing all tasks

---
+++ src/pages/app/taskList.vue
@@ -0,0 +1,13 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col>
+                <h5 class="text-h5 font-weight-bold">Task List 🚀</h5>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script setup>
+
+</script>
