491cf6b23d6e8d13677caf525658c3ddab844829
Ejaz Ahmed <ejaz.ahmed@codenicely.in>
2025-05-27 18:00:58 +0530

    feature: close group modal when user successfully create group

--- src/pages/app/home/project.vue
+++ src/pages/app/home/project.vue
@@ -3,10 +3,11 @@ import { reactive } from "vue";
 import { ref } from "vue";
 import request from "@/plugins/axios";
 import { GET_PROJECT } from "@/constants/apis";
-import { useRoute } from "vue-router";
+import { useRoute,useRouter } from "vue-router";
 import AddEditGroupModal from "@/components/dialogs/AddEditGroupModal.vue";
 import GroupCard from "@/components/cards/GroupCard.vue";
 const route = useRoute();
+const router=useRouter()
 const addGroupDialog = reactive({
   show: false,
   title: "Add Group",
@@ -46,11 +47,14 @@ getProjectDetail();
 <template>
   <v-container>
     <v-row class="d-flex justify-space-between">
-      <v-col cols="12" sm="6" md="4" lg="6">
-        <h5 class="text-h5 font-weight-bold">{{ project.project_name }} 🚀</h5>
-        <p class="text-subtitle-2 text-medium-emphasis">
-          {{ project.description }}
-        </p>
+      <v-col cols="12" sm="6" md="4" lg="6" class="d-flex gap-2">
+        <v-icon size="30" icon="mdi-arrow-left-circle-outline" @click="router.back()"></v-icon>
+        <div>
+          <h5 class="text-h5 font-weight-bold">{{ project.project_name }} 🚀</h5>
+          <p class="text-subtitle-2 text-medium-emphasis">
+            {{ project.description }}
+          </p>
+        </div>
       </v-col>
       <v-col
         cols="12"
@@ -86,9 +90,9 @@ getProjectDetail();
       <v-col v-for="group in project.project_groups" :key="group.id" cols="12">
         <GroupCard :group="group" variant="flat" :showProgress="true">
           <section>
-            <p class="text-h6">Pending Tasks</p>
             <v-list>
-              <v-list-item v-for="task in group?.pending_tasks" :key="task.id">
+              <p class="text-h6">Pending Tasks</p>
+              <v-list-item v-for="task in group?.pending_tasks" :key="task.id" >
                 <TaskCard
                   :editable="true"
                   :task="task"
@@ -158,7 +162,7 @@ getProjectDetail();
       @close="addGroupDialog.show = false"
       :title="addGroupDialog.title"
       :data="addGroupDialog.data"
-      @success="getProjectDetail"
+      @success="addGroupDialog.show = false,getProjectDetail()"
     />
     <AddEditMembersModal
       v-model="addMembersDialog.show"
